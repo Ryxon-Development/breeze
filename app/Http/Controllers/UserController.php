5059363e56a81c0a1bd3f4383a1410daf3a7ef5e
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-03 17:28:44 +0100

    UserController with resource methods created.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //get all users and pass them to the view
+        $users = User::all();
+        return view('users.index', ['users' => $users]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //save the new user
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return redirect()->route('users.index')->with('message', 'User created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //get current users and pass them to the view
+        $user = User::find($id);
+        return view('users.edit', ['user' => $user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        if ($id == 1) {
+            return redirect()->route('users.index')->with('error', 'You cannot delete the admin user!');
+        }
+
+        //delete the user
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect()->route('users.index')->with('message', 'User deleted successfully!');
+    }
+}
