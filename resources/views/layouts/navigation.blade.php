da727be068a9a6ce1c669f2257a6368db92e8fce
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-03 17:32:55 +0100

    submenu, users menu item, and language buttons added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white white:bg-gray-800 border-b border-gray-100 white:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white white:bg-gray-800 border-b border-gray-100 white:border-gray-700 pt-4">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -15,11 +15,24 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                        {{ __('Users') }}
+                    </x-nav-link>
                 </div>
             </div>
 
+{{--            {{ __('test.msg') }}--}}
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="space-x-2 ml-auto mr-0">
+                    <a href="{{ url('lang/nl') }}" class="text-gray-500 hover:text-gray-700 white:text-gray-400 white:hover:text-gray-300 @if(app()->getLocale() === 'nl') font-semibold underline @endif">
+                        NL
+                    </a>
+                    <a href="{{ url('lang/en') }}" class="text-gray-500 hover:text-gray-700 white:text-gray-400 white:hover:text-gray-300 @if(app()->getLocale() === 'en') font-semibold underline @endif">
+                        EN
+                    </a>
+                </div>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 white:text-gray-400 bg-white white:bg-gray-800 hover:text-gray-700 white:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -32,8 +45,8 @@
                             </div>
                         </button>
                     </x-slot>
-
                     <x-slot name="content">
+
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
@@ -42,14 +55,13 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
+
             </div>
 
             <!-- Hamburger -->
@@ -62,7 +74,10 @@
                 </button>
             </div>
         </div>
+
     </div>
+
+    {{ $submenu ?? '' }}
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
@@ -97,4 +112,6 @@
             </div>
         </div>
     </div>
+
 </nav>
+
