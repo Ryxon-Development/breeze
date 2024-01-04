63f6ee0a0bbd7d02b14b2265cf0b59de30a88b79
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-04 14:34:07 +0100

    Added users data and admin to users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -20,6 +23,15 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        //create first tester admin user
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@tester.com',
+            'password' => Hash::make('p4ssw0rd'), // Hash the password
+        ]);
+        //create another 4 random users
+        User::factory()->count(4)->create();
     }
 
     /**
