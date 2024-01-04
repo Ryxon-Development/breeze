781e4108a988dc3a544668d8dde58e66c93f9e5f
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-04 14:33:25 +0100

    Task statuses table migration

---
+++ database/migrations/2024_01_04_114213_add_task_status.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //create table task_status with fields id, label
+        Schema::create('task_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+        });
+
+        //run TaskStatusSeeder
+        $seeder = new \Database\Seeders\TaskStatusSeeder();
+        $seeder->run();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //remove table task_status
+        Schema::dropIfExists('task_status');
+    }
+};
