aa67f7c76fe418f80300472c85e798375914ad42
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-08 13:58:34 +0100

    taskstatus label + assigned header

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -19,9 +19,8 @@
                         <tr>
                             <th class="px-4 py-2">{{ __('ID') }}</th>
                             <th class="px-4 py-2">{{ __('Name') }}</th>
-{{--                            <th class="px-4 py-2">{{ __('Description') }}</th>--}}
                             <th class="px-4 py-2">{{ __('Status') }}</th>
-                            <th class="px-4 py-2">{{ __('User') }}</th>
+                            <th class="px-4 py-2">{{ __('Assigned') }}</th>
                             <th class="px-4 py-2">{{ __('Created at') }}</th>
                             <th class="px-4 py-2">{{ __('Updated at') }}</th>
                             <th class="px-4 py-2">{{ __('Actions') }}</th>
@@ -29,22 +28,29 @@
                         </thead>
                         <tbody>
                         @foreach($tasks as $task)
-                            <tr>
+                            <tr style="background-color:
+                           @if($task->priority === 1)
+                                #ccffcc; /* Light Green */
+                            @elseif($task->priority === 2)
+                                #ffffcc; /* Light Yellow */
+                            @elseif($task->priority === 3)
+                                #ffcccc; /* Light Red */
+                            @else
+                                #FFFFFF; /* White or any other default color */
+                            @endif">
                                 <td class="border px-4 py-2">{{ $task->id }}</td>
                                 <td class="border px-4 py-2">{{ $task->name }}</td>
-{{--                                <td class="border px-4 py-2">{{ $task->description }}</td>--}}
-                                <td class="border px-4 py-2">{{ $task->status }}</td>
+                                <td class="border px-4 py-2">{{ $task->taskStatus->label }}</td>
                                 <td class="border px-4 py-2">{{ $task->user->name }}</td>
                                 <td class="border px-4 py-2">{{ $task->created_at }}</td>
                                 <td class="border px-4 py-2">{{ $task->updated_at }}</td>
                                 <td class="border px-4 py-2">
-{{--                                    <a href="{{ route('tasks.show', $task->id) }}" class="text-blue-600 hover:text-blue-900 mb-2 mr-2">{{ __('View') }}</a>--}}
                                     <a href="{{ route('tasks.edit', $task->id) }}" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">{{ __('Edit') }}</a>
-                                <form class="inline" action="{{ route('tasks.destroy', $task->id) }}" method="POST" onsubmit="return confirm('{{ __('Are you sure you want to delete this task?') }}');">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2">{{ __('Delete') }}</button>
-                                </form>
+                                    <form class="inline" action="{{ route('tasks.destroy', $task->id) }}" method="POST" onsubmit="return confirm('{{ __('Are you sure you want to delete this task?') }}');">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2">{{ __('Delete') }}</button>
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
