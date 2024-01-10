e3001d659b903797a1bef89d435cf2339c6d149c
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-10 12:59:20 +0100

    Admin routes added to auth-group

--- routes/web.php
+++ routes/web.php
@@ -15,36 +15,6 @@ use App\Http\Controllers\TaskController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('lang/nl', [LanguageController::class, 'dutch'])->name('lang.nl');
-Route::get('lang/en', [LanguageController::class, 'english'])->name('lang.en');
-
-//Users
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/{id}/show', [UserController::class, 'show'])->name('users.show');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-//create
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-
-//---TASKS---
-//index
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-//create
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-//show
-Route::get('/tasks/{task}/show', [TaskController::class, 'show'])->name('tasks.show');
-//edit
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-//delete
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy'); //using {task} instead of {id} because of Route Model Binding
-
-
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -57,6 +27,33 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('lang/nl', [LanguageController::class, 'dutch'])->name('lang.nl');
+    Route::get('lang/en', [LanguageController::class, 'english'])->name('lang.en');
+
+//Users
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{id}/show', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+//create
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+//---TASKS---
+//index
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+//create
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+//show
+    Route::get('/tasks/{task}/show', [TaskController::class, 'show'])->name('tasks.show');
+//edit
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+//delete
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy'); //using {task} instead of {id} because of Route Model Binding
 });
 
 require __DIR__.'/auth.php';
