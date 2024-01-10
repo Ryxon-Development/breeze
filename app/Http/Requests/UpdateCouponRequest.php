15f95a0778922fdc4067719d0fca9e941dbfd748
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-10 17:31:41 +0100

    rules and checks for updating coupon requests

--- app/Http/Requests/UpdateCouponRequest.php
+++ app/Http/Requests/UpdateCouponRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class UpdateCouponRequest extends FormRequest
 {
@@ -11,7 +14,16 @@ class UpdateCouponRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'error' => $validator->errors(),
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
     }
 
     /**
@@ -19,10 +31,39 @@ class UpdateCouponRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'value' => 'numeric',
+            'active' => 'boolean',
+            'used' => 'boolean'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return [
+            'value.required' => 'A value is required.',
+            'value.numeric' => 'Value must be numeric',
+            'unique' => 'Coupon code already exists'
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes()
+    {
+        return [
+            'value' => 'Coupon Value',
+            // Add more custom attributes as needed
         ];
     }
 }
