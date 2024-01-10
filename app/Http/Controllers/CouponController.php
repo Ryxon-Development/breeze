896e0a8b4df8b5ab5d692c658706e065029184ce
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-10 17:30:37 +0100

    index, store, update, activate methods.

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -5,15 +5,30 @@ namespace App\Http\Controllers;
 use App\Models\Coupon;
 use App\Http\Requests\StoreCouponRequest;
 use App\Http\Requests\UpdateCouponRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Validation\Validator;
 
 class CouponController extends Controller
 {
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'error' => $validator->errors(),
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $coupons = Coupon::all();
+        return response()->json([
+            'coupons' => $coupons,
+        ], 200);
     }
 
     /**
@@ -21,7 +36,7 @@ class CouponController extends Controller
      */
     public function create()
     {
-        //
+
     }
 
     /**
@@ -29,7 +44,28 @@ class CouponController extends Controller
      */
     public function store(StoreCouponRequest $request)
     {
-        //
+//        //check if coupon already exists
+//        $coupon = Coupon::where('code', $request->code)->first();
+//
+//        if($coupon){
+//            return response()->json([
+//                'error' => 'Coupon already exists',
+//                'coupon' => $coupon
+//            ], 422);
+//        }
+        //This check is now handled by the StoreCouponRequest class in rules()
+
+        //create coupon
+        $coupon = Coupon::create([
+            'code' => $request->code,
+            'value' => $request->value,
+            'used' => '0',
+            'active' => '0'
+        ]);
+        return response()->json([
+            'message' => 'Coupon created successfully',
+            'coupon' => $coupon,
+        ], 200);
     }
 
     /**
@@ -37,7 +73,9 @@ class CouponController extends Controller
      */
     public function show(Coupon $coupon)
     {
-        //
+        return response()->json([
+            'coupon' => $coupon,
+        ], 200);
     }
 
     /**
@@ -53,7 +91,13 @@ class CouponController extends Controller
      */
     public function update(UpdateCouponRequest $request, Coupon $coupon)
     {
-        //
+        //only update coupon values that are passed in request
+        $update = $coupon->update($request->all());
+
+        return response()->json([
+            'message' => 'Coupon updated successfully',
+            'coupon' => $coupon,
+        ], 200);
     }
 
     /**
@@ -62,5 +106,15 @@ class CouponController extends Controller
     public function destroy(Coupon $coupon)
     {
         //
+    }
+
+    public function activate(Coupon $coupon)
+    {
+        $coupon->active = 1;
+        $coupon->save();
+        return response()->json([
+            'message' => 'Coupon activated successfully',
+            'coupon' => $coupon,
+        ], 200);
     }
 }
