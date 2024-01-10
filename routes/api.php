3f4ca32e69f402f12b3f699e9aaa1f63f645c917
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-10 13:00:19 +0100

    Secure api routes added to auth:sanctum, generate-api-key route for logged in users.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Coupon;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/test', function(){
+        $response = json_encode([
+            'message' => 'Successfully authenticated!',
+        ], 200);
+        return response($response, 200)->header('Content-Type', 'application/json');
+    });
+
+    Route::get('/coupons/{coupon:code}', function(Coupon $coupon){
+        return response()->json([
+            'coupon' => $coupon,
+        ], 200);
+    });
+    Route::get('/coupons', function(){
+        $coupons = App\Models\Coupon::all();
+        return response()->json([
+            'coupons' => $coupons,
+        ], 200);
+    });
 });
+
+Route::middleware('auth')->group(function () {
+   Route::put('/generate-token/{user}', [UserController::class, 'generateApiToken'])->name('generate-token');
+});
+
+Route::put('/generate-token/{user}', [UserController::class, 'generateApiToken'])->name('generate-token');
+
+Route::get('/hello', function () {
+
+    //get logged in user
+    $user = User::find(1);
+//    $token = $user->createToken('token-name')->plainTextToken;
+    //generate api_token for user
+    $token = $user->createToken('token-name')->plainTextToken;
+    $user->api_token = $token;
+    $user->save();
+
+    return response()->json([
+        'message' => 'Hello World!',
+    ], 200);
+});
+
