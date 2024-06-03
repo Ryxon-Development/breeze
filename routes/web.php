9f2331b0afb3424b67adb7fd31d9e6261b62b2a8
Ryan Hendriks <ryanhendriks@live.nl>
2024-06-03 13:01:08 +0200

    -reformatted web routes.

--- routes/web.php
+++ routes/web.php
@@ -32,28 +32,32 @@ Route::middleware('auth')->group(function () {
     Route::get('lang/nl', [LanguageController::class, 'dutch'])->name('lang.nl');
     Route::get('lang/en', [LanguageController::class, 'english'])->name('lang.en');
 
-//Users
+    //---USERS---
+    //index
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    //show
     Route::get('/users/{id}/show', [UserController::class, 'show'])->name('users.show');
-    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-//create
+    //create
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    //edit
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    //delete
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
-//---TASKS---
-//index
+    //---TASKS---
+    //index
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-//create
+    //show
+    Route::get('/tasks/{task}/show', [TaskController::class, 'show'])->name('tasks.show');
+    //create
     Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
     Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-//show
-    Route::get('/tasks/{task}/show', [TaskController::class, 'show'])->name('tasks.show');
-//edit
+    //edit
     Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
     Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-//delete
+    //delete
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy'); //using {task} instead of {id} because of Route Model Binding
 });
 
